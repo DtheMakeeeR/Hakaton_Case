56094f89df09a3fd6e4d27bd9a3aa3f8751e10e3
DtheMakeeeR <danilgol45@mail.ru>
2024-10-27 09:29:34 +0300

    Release

--- UI/Src/AI_script.py
+++ UI/Src/AI_script.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import pickle
+from matplotlib import pyplot as plt
+import seaborn as sns
+import plotly.graph_objects as go
 from sklearn.linear_model import LogisticRegression
 
 # выгрузка таблицы в скрипт
@@ -13,16 +16,16 @@ with open("client_prediction_model.pkl", 'rb') as file:
 # предсказание
 y_predict = model.predict(X_test)
 
-produce = {
-    "bad" : 0,
-    'kinda bad' : 1,
-    'not bad' : 2,
-    'norm' : 3,
-    'maybe' : 4,
-    'good' : 5,
-    'excelent' : 6,
-    'very good' : 7
-}
+# produce = {
+#     "bad" : 0,
+#     'kinda bad' : 1
+#     'not bad' : 2,
+#     'norm' : 3,
+#     'maybe' : 4,
+#     'good' : 5,
+#     'excelent' : 6,
+#     'very good' : 7
+# }
 
 #ans = sorted([produce[i] for i in y_predict])
 data = {
@@ -30,6 +33,8 @@ data = {
     "Predicted number": y_predict
     }
 df = pd.DataFrame(data)
-df_sorted = df.sort_values(by="Predicted number")
-df_sorted.to_excel("ModelResult.xlsx")
+df['Predicted number'] = df['Predicted number'].apply(lambda x: {'bad' : 0.9,'kinda bad' : 0.8,'not bad' : 0.7,'norm' : 0.5,'maybe' : 0.4,'good' : 0.3,'excelent' : 0.1,'very good' : 0.01}.get(x))
+df = df.sort_values(by='Predicted number', ascending=False)
+df = df [['ID','Predicted number']]
+df.to_excel('mmodel.xlsx', index=False)
 # ans - output скрипта
